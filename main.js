a7dcdd905d3e6ec8716173b6cd5aa9829f975450
MV3X <mv3x@.ru>
2022-12-24 00:29:24 +0500

    two

--- main.js
+++ main.js
@@ -5,23 +5,19 @@ var vozrast = prompt('Введите свой возраст')
 console.log(vozrast);
 
 var plus = prompt('5 + 5 =')
-console.log('5 + 5 = ' + (5 + 5));
-console.log('Ваш ответ ' + plus);
+console.log('Пример 1: ' + ' 5 + 5 = ' + (5 + 5) + ' Ваш ответ ' + plus);
+
 
 var minus = prompt('10 - 5 =')
-console.log('10 - 5 = ' + (10 - 5));
-console.log('Ваш ответ ' + minus);
+console.log('Пример 2: ' + ' 10 - 5 = ' + (10 - 5) + ' Ваш ответ ' + minus);
 
 var umnojit = prompt('10 * 2 =')
-console.log('10 * 2 = ' + 10 * 2);
-console.log('Ваш ответ ' + umnojit);
+console.log('Пример 3: ' + ' 10 * 2 = ' + 10 * 2 + ' Ваш ответ ' + umnojit);
 
 var delit = prompt('10 / 2 =')
-console.log('10 / 2 = ' + 10 / 2);
-console.log('Ваш ответ ' + delit);
+console.log('Пример 4: ' + ' 10 / 2 = ' + 10 / 2 + ' Ваш ответ ' + delit);
 
 var procent = prompt('10 % 2 =')
-console.log('10 % 2 = ' + 10 % 2);
-console.log('Ваш ответ ' + procent);
+console.log('Пример 5: ' + ' 10 % 2 = ' + 10 % 2 + ' Ваш ответ ' + procent);
 
 alert('Зайдите в консоль для просмотра результатов')
