7e030df1eae12d1cc864c491ca1adcac25a5cfb9
MV3X <mv3x@.ru>
2022-12-24 00:14:46 +0500

    save

---
+++ main.js
@@ -0,0 +1,27 @@
+var imya = prompt('Введите свое имя')
+console.log(imya);
+
+var vozrast = prompt('Введите свой возраст')
+console.log(vozrast);
+
+var plus = prompt('5 + 5 =')
+console.log('5 + 5 = ' + (5 + 5));
+console.log('Ваш ответ ' + plus);
+
+var minus = prompt('10 - 5 =')
+console.log('10 - 5 = ' + (10 - 5));
+console.log('Ваш ответ ' + minus);
+
+var umnojit = prompt('10 * 2 =')
+console.log('10 * 2 = ' + 10 * 2);
+console.log('Ваш ответ ' + umnojit);
+
+var delit = prompt('10 / 2 =')
+console.log('10 / 2 = ' + 10 / 2);
+console.log('Ваш ответ ' + delit);
+
+var procent = prompt('10 % 2 =')
+console.log('10 % 2 = ' + 10 % 2);
+console.log('Ваш ответ ' + procent);
+
+alert('Зайдите в консоль для просмотра результатов')
